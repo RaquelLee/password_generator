cc2da01ff94aeb7f54c7aea3ca9e1720a8865201
rsco <RaquelLee@users.noreply.github.com>
2021-01-06 15:56:23 -0500

    prompt to set user length and while loop to validate

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
 // button element stored into variable for reference
 var generateBtn = document.querySelector("#generate");
 
+// function to collect, validate, and store user choices for reference
+function userChoices () {
+
+  var userLength = parseInt(prompt("Choose a number for characters in your password between 8 and 128"));
+  
+  while (userLength < 8 || userLength > 128 || isNaN(userLength)) {
+    alert("For security purposes, a length of 8-128 must be chosen.");
+    userLength = parseInt(prompt("Choose a number of characters in your password between 8 and 128"));
+  } 
+
+  }
+  
 // function to write password to the #password input
 function writePassword() {
 // function stored into variable for reference 
