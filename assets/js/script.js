0ff5f76b422879402328fbf3338f947a0b7b500d
rsco <RaquelLee@users.noreply.github.com>
2021-01-06 15:48:34 -0500

    comments added to describe starter code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
+// button element stored into variable for reference
 var generateBtn = document.querySelector("#generate");
 
+// function to write password to the #password input
 function writePassword() {
+// function stored into variable for reference 
   var password = generatePassword();
+// textarea element stored into variable for reference
   var passwordText = document.querySelector("#password");
-
+// generated password gets passed into textarea
   passwordText.value = password;
 
 }
 
+// adds event listener to generate button, runs function
 generateBtn.addEventListener("click", writePassword);
